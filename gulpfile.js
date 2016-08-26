6e48e12866a7f5e0ea82e3c03789790106804118
Olivier Van Aken <olivier.va@outlook.com>
2016-08-26 06:53:06 +0200

    setup sass transpilation, watchers and server/browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,46 @@ const sourcemaps = require('gulp-sourcemaps');
 const typescript = require('gulp-typescript');
 const tsconfig = require('./tsconfig.json');
 
+const sass = require('gulp-sass');
+
 gulp.task('clean', function () {
-    return del('dist/**/*');
+    return del('dist');
 });
 
-gulp.task('inject:css', function () {
-    //
+gulp.task('transpile:scss', function () {
+    gulp.src('app/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/app'));
 });
-gulp.task('inject', function () {
-    gulp.start('inject:css');
-});
-
 gulp.task('transpile:ts', function () {
     return gulp
-        .src("app/**/*.ts")
+        .src('app/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(typescript(tsconfig.compilerOptions))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/app'));
 });
 gulp.task('transpile', function () {
+    gulp.start('transpile:scss');
     gulp.start('transpile:ts');
 });
 
+gulp.task('watch:scss', function () {
+    gulp.watch('app/**/*.scss', ['transpile:scss']);
+});
+gulp.task('watch:ts', function () {
+    gulp.watch('app/**/*.ts', ['transpile:ts']);
+});
+gulp.task('watch', function () {
+    gulp.start('watch:scss');
+    gulp.start('watch:ts');
+});
+
 gulp.task('compile', function () {
-    gulp.start('inject');
     gulp.start('transpile');
 });
 gulp.task('build', ['clean'], function () {
     gulp.start('compile');
+    gulp.start('watch');
 });
 gulp.task('default', ['build']);
