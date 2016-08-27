c4e4293fd2d8d2b5fed5f87044ac768ccb38488e
Olivier Van Aken <olivier.va@outlook.com>
2016-08-27 20:45:54 +0200

    removed reset.css

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'campus-info',
     templateUrl: 'app/app.html',
-    styleUrls: ['dist/app/reset.css', 'dist/app/app.css']
+    styleUrls: ['dist/app/app.css']
 })
 
 export class AppComponent { }
